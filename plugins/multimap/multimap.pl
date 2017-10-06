0e20c9668ca44a2b8aba1ac38c9e94b99d4d4c56
yon2kong <yon2kong@gmail.com>
2017-10-06 13:03:05 +0800

    rewrite multimap

--- plugins/multimap/multimap.pl
+++ plugins/multimap/multimap.pl
@@ -1,11 +1,12 @@
 package multimap;
 use strict;
 
-use Globals qw/%config $net %timeout_ex $field/;
+use Globals qw/%config $net %timeout_ex $field $char/;
 use Log qw/message/;
 use Translation qw/T TF/;
 use Commands;
 use Misc qw(offlineMode configModify);
+use Data::Dumper;
 
 Plugins::register('multimap', 'multimap', \&on_unload);
 
@@ -19,7 +20,11 @@ my $chooks = Commands::register(
 	['smap', "State lockmap", \&state_lockmap],
 );
 
-my $last_lock_map;
+my $current_multimap_index = 0;
+my $current_multimap_lvmin = 0;
+my $current_multimap_lvmax = 0;
+my @current_multimap = ();
+
 my $last_interval = 0;
 my $last_change_time = 0;
 my $finish_count = 0;
@@ -29,54 +34,31 @@ sub on_unload {
 	Commands::unregister($chooks);
 }
 
-sub mainLoop_pre {
-	return unless $field;
-	return unless $config{'multiMap'};
-	return if time() < $last_change_time + $last_interval;
+sub find_sutible_multimap {
+	my $lv = shift;
 
-	if ($net && $net->getState == Network::NOT_CONNECTED) {
-		Commands::run("connect");
-		return;
+	for (my $i = 0; $i < $config{'multiMapNr'}; $i++) {
+		next unless $config{"multiMap$i"};
+		my @multimap = split(/ /, $config{"multiMap$i"});
+		my $lvmin = shift @multimap;
+		my $lvmax = shift @multimap;
+		return ($i, $lvmin, $lvmax, @multimap) if $lv >= $lvmin and $lv < $lvmax;
 	}
 
-	calc_next_change_time();
-
-	# init first lock map
-	if (!$last_lock_map) {
-		if ($config{'multiMap'} =~ $field->baseName) {
-			configModify('lockMap', $field->baseName);
-			$last_lock_map = $config{'lockMap'};
-			message TF("lockMap init to current map %s\n", $config{'lockMap'}), "system";
-		} else {
-			change_lockmap();
-		}
-		$finish_count = 0;
-		goto out;
-	}
-
-	finish_lockmap();
-	goto out if ($net && $net->getState == Network::NOT_CONNECTED);
-
-	change_lockmap();
-	Commands::run("autostorage") if $config{'multiMapStorage'};
-
-out:
-	state_lockmap();
+	return (0, 0, 0, ());
 }
 
 sub change_lockmap {
-	my @lockmap = split(/ /, $config{'multiMap'});
 	my $randmap;
 
-	if ($config{'multiMap'} eq $config{'lockMap'}) {
-		message TF("lockMap not changed as it equals to multiMap\n"), "system";
+	if ($#current_multimap == 1) {
+		message TF("lockMap not changed as only one map in multimap\n"), "system";
 	} else {
-		while (($randmap = $lockmap[int(rand(@lockmap))]) eq $config{'lockMap'}) {
+		while (($randmap = $current_multimap[int(rand(@current_multimap))]) eq $config{'lockMap'}) {
 			;#do nothing
 		}
+		#message TF("lockMap changed from '%s' to '%s'\n", $config{'lockMap'}, $randmap), "system";
 		configModify('lockMap', $randmap);
-		message TF("lockMap changed from '%s' to '%s'\n", $last_lock_map, $config{'lockMap'}), "system";
-		$last_lock_map = $config{'lockMap'};
 	}
 }
 
@@ -89,15 +71,65 @@ sub finish_lockmap {
 	}
 }
 
-sub state_lockmap {
-	message TF("lockMap is %s, ", $config{'lockMap'}), "system";
-	message TF("finished count is %d, ", $finish_count), "system";
-	message TF("next change in %d sec\n", $last_interval), "system";
-}
-
 sub calc_next_change_time {
 	$last_interval = $config{'multiMapIntervalMin'} + int(rand($config{'multiMapIntervalSeed'}));
 	$last_change_time = time();
+}
+
+sub state_lockmap {
+	message TF("lockMap is %s, ", $config{'lockMap'}), "system";
+	message TF("finished count is %d, ", $finish_count), "system";
+	message TF("next change in %d sec\n", $last_change_time+$last_interval-time), "system";
+}
+
+sub multimap_init {
+	($current_multimap_index, $current_multimap_lvmin, $current_multimap_lvmax,
+		@current_multimap) = find_sutible_multimap($char->{'lv'});
+	if (!@current_multimap) {
+		message TF("can't find sutible multimap for base level %d\n", $char->{'lv'}), "system";
+		message TF("disable multiMap\n"), "system";
+		configModify('multiMapEnable', 0);
+	}
+
+	message TF("use multiMap%d: %s\n", $current_multimap_index,
+		$config{"multiMap$current_multimap_index"}), "system";
+
+	my $tmp = $field->baseName;
+	if (grep(/$tmp/, @current_multimap)) {
+		configModify('lockMap', $tmp);
+		#message TF("lockMap init to current map %s\n", $config{'lockMap'}), "system";
+	} else {
+		change_lockmap();
+	}
+
+	calc_next_change_time();
+	state_lockmap();
+}
+
+sub mainLoop_pre {
+	return unless $field;
+	return unless $char;
+	return unless $config{'multiMapEnable'};
+
+	multimap_init() if !@current_multimap or $char->{'lv'} >= $current_multimap_lvmax;
+
+	return if time() < $last_change_time + $last_interval;
+
+	if ($net && $net->getState == Network::NOT_CONNECTED) {
+		Commands::run("connect");
+		return;
+	}
+
+	# real loop for finish & change map
+	finish_lockmap();
+
+	if ($net && $net->getState != Network::NOT_CONNECTED) {
+		change_lockmap();
+		Commands::run("autostorage") if $config{'multiMapStorage'};
+	}
+
+	calc_next_change_time();
+	state_lockmap();
 }
 
 1;
